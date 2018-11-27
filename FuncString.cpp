40b25a4a7e999b7a47a04ecd1a910a4c2570aa92
Karchage <45166558+Karchage@users.noreply.github.com>
2018-11-27 23:50:13 +0300

    Analog func string

---
+++ FuncString.cpp
@@ -0,0 +1,87 @@
+#include <iostream>
+#include <cstring>
+//#include <string>
+#include <stdio.h>
+void StrLen() // Dlinna string
+{
+	int size=0;
+	std::string str;
+	std::cout<<"Please write string: ";
+	std::cin>>str;
+	for(int i=0;str[i]!='\0';i++)
+	{
+		size++;
+	}	
+	std::cout<<"String size: "<<size;
+}
+void StrCmp() // Sravnivaet 2 stroki return try if 1 = 2
+{
+	bool ChStr;
+	std::string str1,str2;
+	std::cout<<"Please write string1 and string2: ";
+	std::cin>>str1>>str2;
+	if(str1==str2)
+	{
+		ChStr = true;
+	}	
+	else ChStr = false;
+	std::cout<<"New string: "<<ChStr;
+}
+void StrChr() //Search first simbol and return * = 
+{
+	char simbol;
+	std::string str;
+	char *p;
+	std::cout<<"Please write string and simbol: ";
+	std::cin>>str>>simbol;
+	for(int i=0;str[i]!='\0';i++)
+	{
+		if(str[i]==simbol)
+		{
+			p=&str[i];
+			break;
+		}
+	}
+	std::cout<<p<<"\n"<<*p<<"\n"<<&p<<"\n";
+}
+void StrCpy() // Copy one string to two string
+{
+	std::string str1,str2;
+	std::cout<<"Please write string: ";
+	std::cin>>str1;
+	str2=str1;
+	std::cout<<"String2: "<<str2;
+}
+void StrCat() // soedin 2 string
+{
+	std::string str1,str2;
+	std::cout<<"Please write string1 and string2: ";
+	std::cin>>str1>>str2;
+	str1+=str2;
+	std::cout<<"New string: "<<str1;
+}
+int main ()
+{
+	int a;
+	std::cout<<"1) strlen\n"<<"2) strcpy\n"<<"3) strcmp\n"<<"4) strcat\n"<<"5) strchr\n";
+	std::cin>>a;
+	switch(a)
+	{
+		case 1:
+			StrLen();
+			break;
+		case 2:
+			StrCpy();
+			break;
+		case 3:
+			StrCmp();
+			break;
+		case 4:
+			StrCat();
+			break;
+		case 5:
+			StrChr();
+			break;
+	}
+  return 0;
+}
